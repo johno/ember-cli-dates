eb39bc9ac43864f11a495a1817d20345558d75e0
John Otander <johnotander@gmail.com>
2015-01-18 21:19:25 -0700

    Error formatting

--- addon/utils/valid-args.js
+++ addon/utils/valid-args.js
@@ -2,6 +2,6 @@ import Ember from 'ember';
 
 export default function validArgs(args, helper) {
   if (Ember.isEmpty(args) || args.length === 1) {
-    throw new Ember.Error('[ember-cli-dates:'+helper+'] Invalid number of arguments, expected at least 1');
+    throw new Ember.Error('[ember-cli-dates:' + helper + '] Invalid number of arguments, expected at least 1');
   }
 }
